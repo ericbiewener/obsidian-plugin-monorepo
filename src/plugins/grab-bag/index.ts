ab837264b84a734e813254b66e4f44799cdd9de0
Eric Biewener <eric.biewener0@walmart.com>
2023-12-12 05:12:14 -0800

    toggle list type

--- src/plugins/grab-bag/index.ts
+++ src/plugins/grab-bag/index.ts
@@ -1,12 +1,14 @@
 import * as o from "obsidian";
 import { addInsertTimeHeaderCmd } from "./insert-time-header";
 import { addDemoteHeadingsCmd } from "./demote-headings";
+import { addtoggleListTypeCmd } from "./toggle-list-type";
 
 export default class GrabBagPlugin extends o.Plugin {
   async onload() {
     console.info(`::`, "grab-bag plugin init");
     addInsertTimeHeaderCmd(this);
     addDemoteHeadingsCmd(this);
+    addtoggleListTypeCmd(this);
   }
 
   async onunload() {}
