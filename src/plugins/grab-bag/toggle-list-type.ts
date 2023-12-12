7f19a9b76476ced4e652a5a78170f16d22843298
ericbiewener <ebiewener@gmail.com>
2023-12-12 05:12:51 -0800

    toggle list type (#3)
    Co-authored-by: Eric Biewener <eric.biewener0@walmart.com>

--- src/plugins/grab-bag/toggle-list-type.ts
+++ src/plugins/grab-bag/toggle-list-type.ts
@@ -2,62 +2,21 @@ import * as o from "obsidian";
 import { addCommand } from "../../add-command";
 import { getEditor } from "../../../utils/obsidian/editor/get-editor";
 
-const leadingWhitespace = /^(\s*)/;
 const unorderedListItem = /^(\s*)-( .*)/;
-const orderedListItem = /^(\s*)[0-9]+\.( .*)/;
 
-const orderedListItemMatch = /^\s*([0-9]+)\. /;
-
-const makeUnordered = (editor: o.Editor, line: string, lineNo: number) => {
-  const newLine = line.replace(orderedListItem, "$1-$2");
-  editor.replaceRange(newLine, { line: lineNo, ch: line.length });
-};
-
-const getIndentation = (line: string) =>
-  line.match(leadingWhitespace)[1]?.length || 0;
-
-const findPrevNum = (
-  editor: o.Editor,
-  lineNo: number,
-  indentation: number,
-): number | null => {
-  const line = editor.getLine(lineNo);
-  const newIndentation = getIndentation(line);
-
-  if (newIndentation < indentation) return null;
-
-  if (newIndentation === indentation) {
-    if (unorderedListItem.test(line)) return null;
-    const match = line.match(orderedListItemMatch)[1];
-    return match ? parseInt(match, 10) : null;
-  }
-
-  return findPrevNum(editor, lineNo, indentation);
-};
-
-const makeOrdered = (editor: o.Editor, line: string, lineNo: number) => {
-  const indentation = line.match(leadingWhitespace)[1]?.length || 0;
-  const prevNum = findPrevNum(editor, lineNo - 1, indentation) || 0;
-  // const newLine = line.replace(orderedListItem, "$1-$2")
-  // editor.replaceRange(newLine, { line: lineNo, ch: line.length})
-};
-
-const toggleListType = async ({ app }: o.Plugin, line = 0) => {
+const toggleListType = async ({ app }: o.Plugin) => {
   const editor = getEditor(app);
-  const startLine = editor.getCursor("from").line;
-  const toLine = editor.getCursor("to").line;
+  const lineNo = editor.getCursor("from").line;
+  const line = editor.getLine(lineNo);
 
-  for (let i = startLine; i <= toLine; i++) {
-    const line = editor.getLine(i);
-    if (unorderedListItem.test(line)) {
-      makeOrdered(editor, line, i);
-    } else if (orderedListItem.test(line)) {
-      makeUnordered(editor, line, i);
-    }
-  }
+  app.commands.executeCommandById(
+    unorderedListItem.test(line)
+      ? "editor:toggle-numbered-list"
+      : "editor:toggle-bullet-list",
+  );
 };
 
-export const addInsertTimeHeaderCmd = addCommand(
+export const addtoggleListTypeCmd = addCommand(
   "Toggle list type",
   toggleListType,
 );
--- types/obsidian.d.ts
+++ types/obsidian.d.ts
@@ -1,5 +1,62 @@
-// declare module "obsidian" {
-//   interface App {
-//     newMethod(): void;
-//   }
-// }
+import * as o from "obsidian";
+
+declare module "obsidian" {
+  export interface App {
+    commands: any;
+  }
+
+  export type PluginInstance = {
+    id: string;
+  };
+
+  export type InstalledPlugin = {
+    enabled: boolean;
+    instance: PluginInstance;
+  };
+
+  export type InternalPlugins = {
+    plugins: Record<string, InstalledPlugin>;
+    getPluginById(id: string): InstalledPlugin;
+    getEnabledPluginById(id: string): PluginInstance;
+  };
+
+  export type QuickSwitcherOptions = {
+    showAllFileTypes?: boolean;
+    showAttachments?: boolean;
+    showExistingOnly?: boolean;
+  };
+
+  export type SystemSwitcherClass = typeof SystemSwitcher;
+
+  export type QuickSwitcherPluginInstance = PluginInstance & {
+    options: QuickSwitcherOptions;
+    QuickSwitcherModal: SystemSwitcherClass;
+  };
+
+  interface App {
+    internalPlugins: InternalPlugins;
+  }
+
+  type Suggestion = unknown;
+
+  export interface Chooser<T> {
+    selectedItem: number;
+    suggestions: HTMLDivElement[];
+    values: T[];
+    setSelectedItem(index: number, evt: MouseEvent | KeyboardEvent): void;
+    setSuggestions(suggestions: T[]): void;
+    useSelectedItem(evt: KeyboardEvent): void;
+  }
+
+  export class SystemSwitcher extends o.SuggestModal<Suggestion> {
+    shouldShowAlias: boolean;
+    protected isOpen: boolean;
+    protected chooser: Chooser<Suggestion>;
+    constructor(app: App, opts: QuickSwitcherOptions);
+    protected onInput(): void;
+    protected updateSuggestions(): void;
+    getSuggestions(query: string): Suggestion[];
+    renderSuggestion(value: Suggestion, el: HTMLElement): void;
+    onChooseSuggestion(item: Suggestion, evt: MouseEvent | KeyboardEvent): void;
+  }
+}
