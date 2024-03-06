7cf80265f8d0eaa8ac4d092ff85c94b46d26257a
Eric Biewener <ebiewener@gmail.com>
2024-03-06 10:14:38 -0800

    more - Wed Mar  6 10:14:38 PST 2024

--- src/plugins/grab-bag/toggle-list-type.ts
+++ src/plugins/grab-bag/toggle-list-type.ts
@@ -5,18 +5,18 @@ import { getEditor } from "../../utils/obsidian/editor/get-editor";
 const unorderedListItem = /^(\s*)-( .*)/;
 
 const toggleListType = async ({ app }: o.Plugin) => {
-  const editor = getEditor(app);
-  const lineNo = editor.getCursor("from").line;
-  const line = editor.getLine(lineNo);
+	const editor = getEditor(app);
+	const lineNo = editor.getCursor("from").line;
+	const line = editor.getLine(lineNo);
 
-  app.commands.executeCommandById(
-    unorderedListItem.test(line)
-      ? "editor:toggle-numbered-list"
-      : "editor:toggle-bullet-list",
-  );
+	app.commands.executeCommandById(
+		unorderedListItem.test(line)
+			? "editor:toggle-numbered-list"
+			: "editor:toggle-bullet-list",
+	);
 };
 
 export const addtoggleListTypeCmd = addCommand(
-  "Toggle list type",
-  toggleListType,
+	"Toggle list type",
+	toggleListType,
 );
--- types/obsidian.d.ts
+++ types/obsidian.d.ts
@@ -1,72 +1,72 @@
 import * as o from "obsidian";
 
 declare module "obsidian" {
-  export interface App {
-    commands: {
-      commands: Record<string, o.Command>;
-      // Not visible when logging `app.commands` to the console
-      executeCommandById(id: string): boolean;
-    };
-    plugins: {
-      manifests: o.PluginManifest[];
-      plugins: Record<string, o.Plugin>;
-      // Not visible when logging `app.plugins` to the console
-      enablePlugin(pluginId: string): Promise<void>;
-    };
-  }
+	export interface App {
+		commands: {
+			commands: Record<string, o.Command>;
+			// Not visible when logging `app.commands` to the console
+			executeCommandById(id: string): boolean;
+		};
+		plugins: {
+			manifests: o.PluginManifest[];
+			plugins: Record<string, o.Plugin>;
+			// Not visible when logging `app.plugins` to the console
+			enablePlugin(pluginId: string): Promise<void>;
+		};
+	}
 
-  export type PluginInstance = {
-    id: string;
-  };
+	export type PluginInstance = {
+		id: string;
+	};
 
-  export type InstalledPlugin = {
-    enabled: boolean;
-    instance: PluginInstance;
-  };
+	export type InstalledPlugin = {
+		enabled: boolean;
+		instance: PluginInstance;
+	};
 
-  export type InternalPlugins = {
-    plugins: Record<string, InstalledPlugin>;
-    getPluginById(id: string): InstalledPlugin;
-    getEnabledPluginById(id: string): PluginInstance;
-  };
+	export type InternalPlugins = {
+		plugins: Record<string, InstalledPlugin>;
+		getPluginById(id: string): InstalledPlugin;
+		getEnabledPluginById(id: string): PluginInstance;
+	};
 
-  export type QuickSwitcherOptions = {
-    showAllFileTypes?: boolean;
-    showAttachments?: boolean;
-    showExistingOnly?: boolean;
-  };
+	export type QuickSwitcherOptions = {
+		showAllFileTypes?: boolean;
+		showAttachments?: boolean;
+		showExistingOnly?: boolean;
+	};
 
-  export type SystemSwitcherClass = typeof SystemSwitcher;
+	export type SystemSwitcherClass = typeof SystemSwitcher;
 
-  export type QuickSwitcherPluginInstance = PluginInstance & {
-    options: QuickSwitcherOptions;
-    QuickSwitcherModal: SystemSwitcherClass;
-  };
+	export type QuickSwitcherPluginInstance = PluginInstance & {
+		options: QuickSwitcherOptions;
+		QuickSwitcherModal: SystemSwitcherClass;
+	};
 
-  interface App {
-    internalPlugins: InternalPlugins;
-  }
+	interface App {
+		internalPlugins: InternalPlugins;
+	}
 
-  type Suggestion = unknown;
+	type Suggestion = unknown;
 
-  export interface Chooser<T> {
-    selectedItem: number;
-    suggestions: HTMLDivElement[];
-    values: T[];
-    setSelectedItem(index: number, evt: MouseEvent | KeyboardEvent): void;
-    setSuggestions(suggestions: T[]): void;
-    useSelectedItem(evt: KeyboardEvent): void;
-  }
+	export interface Chooser<T> {
+		selectedItem: number;
+		suggestions: HTMLDivElement[];
+		values: T[];
+		setSelectedItem(index: number, evt: MouseEvent | KeyboardEvent): void;
+		setSuggestions(suggestions: T[]): void;
+		useSelectedItem(evt: KeyboardEvent): void;
+	}
 
-  export class SystemSwitcher extends o.SuggestModal<Suggestion> {
-    shouldShowAlias: boolean;
-    protected isOpen: boolean;
-    protected chooser: Chooser<Suggestion>;
-    constructor(app: App, opts: QuickSwitcherOptions);
-    protected onInput(): void;
-    protected updateSuggestions(): void;
-    getSuggestions(query: string): Suggestion[];
-    renderSuggestion(value: Suggestion, el: HTMLElement): void;
-    onChooseSuggestion(item: Suggestion, evt: MouseEvent | KeyboardEvent): void;
-  }
+	export class SystemSwitcher extends o.SuggestModal<Suggestion> {
+		shouldShowAlias: boolean;
+		protected isOpen: boolean;
+		protected chooser: Chooser<Suggestion>;
+		constructor(app: App, opts: QuickSwitcherOptions);
+		protected onInput(): void;
+		protected updateSuggestions(): void;
+		getSuggestions(query: string): Suggestion[];
+		renderSuggestion(value: Suggestion, el: HTMLElement): void;
+		onChooseSuggestion(item: Suggestion, evt: MouseEvent | KeyboardEvent): void;
+	}
 }
