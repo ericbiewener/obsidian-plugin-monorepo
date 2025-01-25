d8460afe21c299647f5fabc23c89bbffae20f7cd
Eric Biewener <ebiewener@gmail.com>
2025-01-25 10:11:17 -0800

    more - Sat Jan 25 10:11:17 PST 2025

--- src/plugins/utils/obsidian/workspace/track-open-files.ts
+++ src/plugins/utils/obsidian/workspace/track-open-files.ts
@@ -1,5 +1,4 @@
 import * as o from "obsidian";
-import { isDefined } from "../../type-guards/is-defined";
 import { getMarkdownLeaves } from "./get-markdown-leaves";
 
 export const trackOpenFiles = (app: o.App) => {
@@ -9,9 +8,9 @@ export const trackOpenFiles = (app: o.App) => {
 	app.workspace.on("layout-change", () => {
 		const leaves = getMarkdownLeaves(app);
 		const leavesWithNewFiles = leaves.filter(
-			(l) => !openPaths.includes(l.view.file.path),
+			(l) => !openPaths.includes(l.view.file?.path),
 		);
-		openPaths = leaves.map((p) => p.view.file?.path).filter(isDefined);
+		openPaths = leaves.map((p) => p.view.file.path);
 
 		for (const leaf of leavesWithNewFiles) {
 			app.workspace.trigger("file-opened", leaf);
